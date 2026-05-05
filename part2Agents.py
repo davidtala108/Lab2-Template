15d71fa68b86b28b1067a8814af0c49c0155cf14
david108 <datalave@gmail.com>
2026-05-05 15:27:48 +0000

    Puzzel Wizard

--- part2Agents.py
+++ part2Agents.py
@@ -31,42 +31,84 @@ class PuzzleWizard(WizardAgent):
         Passed = 4
         
         s = Solver()
+        fire_positions = {(f.row, f.col) for f in fire_stones}
+        ice_positions = {(ice.row, ice.col) for ice in ice_stones}
         
-        X = [[Int(f"x_{i}_{j}") for j in range(grid_size[1])] for i in range(grid_size[0])]
-        
-        s.add(X[wizard_location.col][wizard_location.row] == START)
-        
-        for f in fire_stones:
-            s.add(X[f.col][f.row] == Turn)
-        
-        for ice in ice_stones:
-            s.add(X[ice.col][ice.row] == Straight)
+        X = [[Int(f"{i}_{j}") for j in range(grid_size[0])] for i in range(grid_size[1])]
 
+        for i in range(grid_size[1]):
+            for j in range(grid_size[0]):
+                if i == wizard_location.col and j == wizard_location.row:
+                    s.add(X[i][j] == START) 
+                
+                elif (i,j) in fire_positions:
+                    s.add(X[i][j] == Turn)
+                
+                elif (i,j) in ice_positions:
+                    s.add(X[i][j] == Straight)
+                    
+        
 
+                    
         match s.check():
             case z3.sat:
-                m = s.model()
-                
+                    m = s.model()
+
+                    grid = [
+                    [
+                    m.evaluate(X[i][j], model_completion=True).as_long()
+                    for j in range(len(X[0]))
+                    ]
+                        for i in range(len(X))
+                            ]
+                    path = []
+                    for i in range(len(grid)):
+                        for j in range(len(grid[0])):
+                            if grid[i][j] != 0:
+                                if grid[i][j] == 1:
+                                    path.append("START")
+                                    SP = len(path)
+                                path.append([i, j])
+
+                    Wiz_Actions = []
+                    while (1):
+                        if (SP + 1) == len(path):
+                            second = 0
+                        else:
+                            second = SP + 1
+                        current = path[SP]
+                        if current == "START":
+                            break
+                        next_stone = path[second]
+                        if next_stone == "START":
+                            break
+                        vertical = path[second][0] - path[SP][0]
+                        horizontal = path[second][1] - path[SP][1]
+
+                        if horizontal > 0:
+                            for k in range(horizontal):
+                                Wiz_Actions.append(WizardMoves.RIGHT)
+                        elif horizontal < 0:
+                            for l in range (horizontal):
+                                Wiz_Actions.append(WizardMoves.LEFT)
+                        if vertical > 0:
+                            for k in range(vertical):
+                                Wiz_Actions.append(WizardMoves.UP)
+                        elif horizontal < 0:
+                            for l in range (vertical):
+                                Wiz_Actions.append(WizardMoves.DOWN)
+                        
+                        SP += 1
+                        if SP == len(path):
+                            SP = 0
+                        
+                        print("VAILD SOLUTION:",Wiz_Actions, "\n")
+                        
+
+                    
+
             case z3.unsat:
                 print("Wizard can't complete this puzzle")            
-            
-
-            
-
-            
-            
-
-
-            
-            
-            
-    
-
-
-
-
-        
-        
         
     
         return MASYU_1_SOLUTION.pop(0)
